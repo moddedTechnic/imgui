a28076f207f1474517d3cf62a1992cd36f1ca401
moddedTechnic <jpleeming51@gmail.com>
2020-06-19 13:43:43 +0100

    Set output and obj dirs

--- premake5.lua
+++ premake5.lua
@@ -2,8 +2,8 @@ project "ImGui"
 	kind "StaticLib"
 	language "C++"
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
 
 	files {
 		"imconfig.h",
