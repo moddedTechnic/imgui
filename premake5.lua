da34140c48e77b3e61db52a4a80289fcd05b2cc6
moddedTechnic <jpleeming51@gmail.com>
2020-06-19 13:43:04 +0100

    Neatened premake

--- premake5.lua
+++ premake5.lua
@@ -5,8 +5,7 @@ project "ImGui"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
+	files {
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
